009ac30fa278aec8f06529a3390cc3b9d3b24141
GabrielMoyaNascimento <gabrielmoya201415@gmail.com>
2023-04-12 20:51:32 -0300

    Adicionando o projeto PHP novamente

--- PHP/app/Http/Controllers/BotManController.php
+++ PHP/app/Http/Controllers/BotManController.php
@@ -13,15 +13,27 @@ class BotManController extends Controller
     public function handle()
     {
         $botman = app('botman');
-   
+        
         $botman->hears('{message}', function($botman, $message) {
    
             if ($message == 'Olá') {
                 $this->askName($botman);
             }
-            
+            if ($message == 'Turing') {
+                $botman->reply('O teste de Turing é baseado em uma conversa entre uma máquina e um ser humano, que deve determinar se as respostas da máquina são ou não indistinguíveis das respostas de um ser humano.');
+            }
+            if ($message == 'IFPR') {
+                $botman->reply('O IFPR campus Paranavaí foi criado em 2008, a partir da expansão da rede federal de educação profissional e tecnológica. Desde então, tem oferecido ensino de qualidade e formado profissionais capacitados para o mercado de trabalho.');
+            }
+
             else{
-                $botman->reply("Diga 'Olá' para iniciar...");
+                $botman->reply("
+                    Olá, digite uma das opções abaixo para que eu possa te auxiliar melhor,
+                    Você deseja:
+                    1 - 'Turing' - Conhecer mais sobre Alan Turing e o teste de turing
+                    2 - 'IFPR' - Saber uma curiosidade sobre o campus   Paranavaí
+                    3 - ' Olá' - Perguntarei o seu nome para te cumprimentar
+                ");
             }
    
         });
@@ -39,6 +51,6 @@ class BotManController extends Controller
             $name = $answer->getText();
    
             $this->say('Prazer em te conhecer '.$name);
-        });
+        });  
     }
 }
